702c12d51372ffc7715e4bddd63b0f74282205c9
Nandhini V.g. <gvns1029@gmail.com>
2026-01-23 04:33:21 +0000

    give it here as text completely

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,17 @@
+'use client';
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCFNj2AhRE8qLS0o_9ir8nM6xLg83BQ4Aw",
-  authDomain: "neuro-5b180.firebaseapp.com",
-  projectId: "neuro-5b180",
-  storageBucket: "neuro-5b180.appspot.com",
-  messagingSenderId: "781267118248",
-  appId: "1:781267118248:web:7c32d4e55e7e7ce88f3a9d",
-  measurementId: "G-CB5GQW1DZ3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
